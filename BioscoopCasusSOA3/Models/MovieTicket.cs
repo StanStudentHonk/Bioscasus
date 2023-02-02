c416068f227acd49b6bb8399dc792b280262692b
KhalidMimouni <74767260+KhalidMimouni@users.noreply.github.com>
2023-02-02 12:00:40 +0100

    hoi stan

---
+++ BioscoopCasusSOA3/Models/MovieTicket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioscoopCasusSOA3.Models
+{
+	public class MovieTicket
+	{
+		private int _rowNr { get; set; }
+		private int _seatNr { get; set; }
+		private bool _isPremium { get; set; }
+
+		public MovieTicket(int rowNr, int seatNr, bool isPremium)
+		{
+			this._rowNr = rowNr;
+			this._seatNr = seatNr;
+			this._isPremium = isPremium;
+		}
+
+		public bool IsPremiumTicket()
+		{
+			return this._isPremium;
+		}
+
+		public double GetPrice()
+		{
+			//TODO: IMPLEMENT METHOD ACCORDING TO DOCUMENT (Zie word bestand)
+			return 1.0;
+		}
+
+
+	}
+}
---
+++ BioscoopCasusSOA3/Models/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioscoopCasusSOA3.Models
+{
+	public class Order
+	{
+		private int _orderNr { get; set; }
+		private bool _isStudentOrder { get; set; }
+		private List<MovieTicket> _movieTickets { get; set; }
+
+		public Order(int orderNr, bool isStudentOrder)
+		{
+			this._orderNr = orderNr;
+			this._isStudentOrder = isStudentOrder;
+			_movieTickets = new List<MovieTicket>();
+		}
+
+		public int GetOrderNr()
+		{
+			return this._orderNr;
+		}
+
+		public void AddSeatReservations(MovieTicket movieTicket)
+		{
+			_movieTickets.Add(movieTicket);
+		}
+
+		public double CalculatePrice()
+		{
+			//todo: implement method
+			return 1.0;
+		}
+
+		public void Export() 
+		{ 
+			
+		}
+
+	}
+}
