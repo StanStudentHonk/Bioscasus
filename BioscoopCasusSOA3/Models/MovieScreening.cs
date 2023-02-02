f06302ca583a5b097d719d1ab96f5676dfefe293
StanStudentHonk <74409816+StanStudentHonk@users.noreply.github.com>
2023-02-02 11:48:03 +0100

    added movieScreening

---
+++ BioscoopCasusSOA3/Models/MovieScreening.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioscoopCasusSOA3.Models
+{
+    public class MovieScreening
+    {
+        private DateTime _dateAndTime { get; set; }
+        private double _pricePerSeat { get;  set; }
+        private Movie _movie { get; set; }
+
+        public MovieScreening(DateTime dateAndTime, double pricePerSeat)
+        {
+            _dateAndTime = dateAndTime;
+            _pricePerSeat = pricePerSeat;
+        }
+
+        public double GetPricePerSeat()
+        {
+            return _pricePerSeat;
+        }
+
+        public override string ToString()
+        {
+            return _pricePerSeat + _dateAndTime.ToString();
+        }
+    }
+}
