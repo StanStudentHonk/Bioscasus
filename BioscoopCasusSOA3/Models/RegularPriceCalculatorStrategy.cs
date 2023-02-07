2f25beb10f771eeccc2ed10089621b89b085d08f
KhalidMimouni <74767260+KhalidMimouni@users.noreply.github.com>
2023-02-07 15:14:43 +0100

    implemented strategy pattern

---
+++ BioscoopCasusSOA3/Models/RegularPriceCalculatorStrategy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioscoopCasusSOA3.Models
+{
+	public class RegularPriceCalculatorStrategy : IPriceCalculatorStrategy
+	{
+		public double GetTotalPriceOfTickets(List<MovieTicket> movieTickets)
+		{
+			double totalPrice = 0;
+
+			for (int i = 0; i < movieTickets.Count; i++)
+			{
+				MovieTicket movieTicket = movieTickets[i];
+				double ticketPrice = movieTicket.GetPrice();
+				
+				if (movieTicket.IsPremiumTicket())
+				{
+					ticketPrice += 3;
+				}
+				
+				DayOfWeek dayOfWeek = movieTicket._movieScreening._dateAndTime.DayOfWeek;
+				bool isWeekDay = dayOfWeek >= DayOfWeek.Monday && dayOfWeek <= DayOfWeek.Thursday;
+				
+				if ((isWeekDay && i % 2 == 0) || !isWeekDay)
+				{
+					totalPrice += ticketPrice;
+				}
+				
+			
+			}
+			
+		
+			totalPrice = movieTickets.Count > 5 ? totalPrice += 0.9 : totalPrice;
+			
+
+			return totalPrice;
+		}
+	}
+}
